9cbf4858f5b23ad9d96a0a21a981a5ed1eaf7712
Hector Manuel Barrios Barrios <barrioshector13@gmail.com>
2022-03-01 12:44:57 -0600

    Read all files with extension .txt, save its content in string, then write the string to a file all.txt

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os, sys
+
 def read_files():
     """
         read_files.
@@ -6,16 +8,31 @@ def read_files():
 
         Returns a string concatenating the content of the .txt files
     """
-    archivo = '2.txt'
-    with open(f'./{archivo}', 'r', encoding='utf-8') as f:
-        text = f.read()
+    files = os.listdir('./')
+    text =''
+    for filename in files:
+        if filename.split('.')[-1] == 'txt':
+            with open(f'./{filename}', 'r', encoding='utf-8') as f:
+                text += str(f.read()) + '\n'
     return text
 
 
-
 def write_file():
-    pass
+    """
+        write_files.
+
+        no input data.
+
+        it does not return data, it just saves all the text in a single .txt file
+    """
+    text = read_files()
+    with open('./all.txt', 'w', encoding='utf-8') as f:
+        f.write(text)
+
+
+def run():
+    write_file()
 
 
 if __name__ == '__main__':
-    print(read_files())
+    run()
